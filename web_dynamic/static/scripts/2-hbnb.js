497e30a8cd0cb2de31e2121901c46b963e47b397
laurendobratz00 <laurendobratz@icloud.com>
2021-05-14 22:48:14 +0000

    add new function

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(document).addEventListener("DOMContentLoaded", function() {
+    $(function () {
+	let amenity_list = []
+	$('input[name=checkbox]').change(function () {
+	        let data_id = $('#popover').get(0).id;
+	        if ($('INPUT').is(':checked')) {
+                amenity_list = amenity_list.push(data_id);
+		        }
+	        else {
+		    amenity_list = amenity_list.filter(":checked")
+		        }
+	        $('DIV.amenities H4').html(amenity_list.join(', '));
+	});
+    });
+});
+$(function() {
+    $.getJSON( "http://0.0.0.0:5001/api/v1/status/", function( data ) {
+	if ($( ".result" ).html( data )) {
+	    $('DIV#api_status').addClass("available");
+	} else {
+	    $('DIV#api_status')removeClass('available');
+	}
+    });
+});
