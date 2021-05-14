5c7701bcfb56ce5e3c0bf95e983c03cbc6d4fe71
laurendobratz00 <laurendobratz@icloud.com>
2021-05-14 17:02:24 +0000

    add js file

---
+++ web_dynamic/static/1-hbnb.js
@@ -0,0 +1,14 @@
+$(document).addEventListener("DOMContentLoaded", function() {
+    $(function () {
+	let amenity_list = []
+	$('input[name=checkbox]').change(function () {
+	    let data_id = $('#popover').get(0).id;
+	    if ($('INPUT').is(':checked')) {
+                amenity_list = amenity_list.push(data_id);
+	    }
+	    else {
+		amenity_list = amenity_list.filter(":checked")
+	    }
+	});
+    });
+});
