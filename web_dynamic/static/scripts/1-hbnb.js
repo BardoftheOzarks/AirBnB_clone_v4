e63b390e4614ee1277b6d1d0114642b92fd492d5
BardoftheOzarks <1886@holbertonschool.com>
2021-05-15 17:59:12 -0500

    corrected spacing

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,14 @@
 $(document).addEventListener("DOMContentLoaded", function() {
     $(function () {
-	let amenity_list = []
-	$('input[name=checkbox]').change(function () {
-	    let data_id = $('#popover').get(0).id;
-	    if ($('INPUT').is(':checked')) {
+		let amenity_list = []
+		$('input[name=checkbox]').change(function () {
+	    	let data_id = $('#popover').get(0).id;
+	    	if ($('INPUT').is(':checked')) {
                 amenity_list = amenity_list.push(data_id);
-	    }
-	    else {
-		amenity_list = amenity_list.filter(":checked")
-	    }
-	    $('DIV.amenities H4').html(amenity_list.join(', '));
-	});
+	    	} else {
+				amenity_list = amenity_list.filter(":checked")
+	    	}
+	    	$('DIV.amenities H4').html(amenity_list.join(', '));
+		});
     });
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,24 +1,23 @@
 $(document).addEventListener("DOMContentLoaded", function() {
     $(function () {
-	let amenity_list = []
-	$('input[name=checkbox]').change(function () {
-	        let data_id = $('#popover').get(0).id;
-	        if ($('INPUT').is(':checked')) {
-                amenity_list = amenity_list.push(data_id);
-		        }
-	        else {
-		    amenity_list = amenity_list.filter(":checked")
-		        }
-	        $('DIV.amenities H4').html(amenity_list.join(', '));
+		let amenity_list = []
+		$('input[name=checkbox]').change(function () {
+	    	let data_id = $('#popover').get(0).id;
+	    	if ($('INPUT').is(':checked')) {
+            	amenity_list = amenity_list.push(data_id);
+			} else {
+		  		amenity_list = amenity_list.filter(":checked")
+			}
+	    	$('DIV.amenities H4').html(amenity_list.join(', '));
+		});
+    });
+	$(function () {
+	    $.getJSON("http://0.0.0.0:5001/api/v1/status/", function( data ) {
+			if (data.status === 'OK') {
+			    $('div#api_status').addClass("available");
+			} else {
+				$('div#api_status').removeClass("available");
+    		}
+		});
 	});
-    });
-});
-$(function() {
-    $.getJSON( "http://0.0.0.0:5001/api/v1/status/", function( data ) {
-	if (data.status === 'OK') {
-	    $('DIV#api_status').addClass("available");
-	} else {
-	    $('DIV#api_status')removeClass('available');
-	}
-    });
 });
